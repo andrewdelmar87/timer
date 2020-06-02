e1d9fa18acf2d8bca0cb4fbd70c6894b17302045
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-02 20:56:33 +0000

    Simple timer

---
+++ timer1.js
@@ -0,0 +1,15 @@
+const alarms = process.argv.slice(2); //arr of nums
+
+const timer = function(array) {
+  for(const time of array){
+    if (time >= 0) {
+    const delay = time * 1000 //convert seconds to ms
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`ALARM!!! at ${time} seconds`);
+    }, delay)
+   } 
+  }
+};
+
+timer(alarms);
